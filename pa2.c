50bbef35023d4552a8d793521535c889388d3dc1
hyokkkk <tahyok7123@naver.com>
2020-10-01 16:03:53 +0900

    encoding

--- pa2.c
+++ pa2.c
@@ -34,7 +34,8 @@
 //          -> 1.1111100100 * 2^10
 // 2. ROUNDING 
 //      1) frac bit = 5bit. 
-//      2) LSB = 101이므로 걍 truncate한다.
+//      2) LRS = 011, 111, 110 인 경우에만 round up, 나머지는 truncate.
+//          -> LRS = 101이므로 걍 truncate한다.
 //          -> 1.11111 * 2^10
 // 3. renormalize : 만일 10.00000 * 2^31 과 같이 노멀이 아닌 값이 나오면 1.00000 * 2^32로 normalize.
 // 4. encoding
@@ -53,12 +54,13 @@
 /* Convert 32-bit signed integer to 12-bit floating point */
 fp12 int_fp12(int n)
 {
-    // n == 0 일 때 처리
+
+// -1. n == 0 일 때 처리
     if (n == 0) return 0;
 
-    //
-    // 0. sign 받아놓기
-    //
+//
+// 0. sign 받아놓기
+//
     unsigned short int sign = n < 0 ? 0xf800 : 0; // -: 1111 1000 0000 0000, 0 & +: 00000000 00000000
     unsigned int un= n < 0 ? (unsigned int) ~n+1 : n; //음수면 양수로 바꿈. -2147483648같은 경계값은 -붙여도 자기 자신임. 
 
@@ -66,9 +68,9 @@ fp12 int_fp12(int n)
     //unsigned int un =(unsigned int)n ;
 
 
-    //
-    // 1. E를 구하라
-    //
+//
+// 1. Normalizing & E 구하기
+//
 
     //TODO : delete
     printf("n : %d, un : %u, 맨앞1: %u\n", n, un, (unsigned int) 0x80000000);
@@ -76,21 +78,70 @@ fp12 int_fp12(int n)
 
     int cnt = 1;
     
+    // un의 MSB == 1일 때까지 shift
     while (un < (unsigned int)0x80000000){
         un<<=1; cnt++;
-    } // 여기까지 했으면 un의 MSB == 1 이다.
-    
+    } 
+
     int e = 32 - cnt;
 
     //TODO : delete
     printf("e : %d\n", e);
 
-    //
-    // 2. rounding
-    //
 
+//
+// 2. Rounding : LRS = 011, 111, 110 일 때만 +1하고 나머지는 truncate한다.
+//
+    //
+    // 2-1) RS == 11 check
+    //
+    unsigned int rs = 0xffffffff >> 6; // int 1bit + frac 5 bit -> 00000LR111111111...
+    
+    // rs & un 한 게 >= 00000010 00000000 00000000 00000001 이면 s==1 이라는 의미다. 
+    bool RS = rs & un > 0x02000000 ? true : false;
+
+
+    //
+    // 2-2) LR == 11 check
+    //
+    unsigned int lr = 0xffffffff >> 5; // int 1bit + frac 5bit 중 LSB니까 5bit shift -> 00000111.....
+    
+    // lr & un >= 000001100 00000000 00000000 00000000 이면 s에 상관 없이 LR == 11임.
+    bool LR = lr & un >= 0x0c000000 ? true : false;
+
+
+    //
+    // 2-3) truncate 후 LRS 조건에 맞는 것만 +1
+    //
+    un >>= 32-6; // int 1bit + frac 5bit = 6bit만 남게 shift 
+    if (RS || LR) un += 1; 
+
+
+//
+// 3. Renormalization
+// -> 정상이라면 1xxxxx처럼 6bit 숫자임. but 10.00000처럼 7bit로 넘어가는 경우도 있다.
+// -> 01000000 이상이면 >>1 하고 e += 1;
+//
+    
+    if (un >= 0x40) {
+        un >>= 1;
+        e += 1;
+    }
+
+
+//
+// 4. Encoding
+// -> sign, exp, frac을 애초에 16bit로 extend해서 선언해놓고 result에 넣을 때 shift 없이 OR만 한다.
+// 
+    
+    // 4-1) sign은 위에서 구해놓음
+    
+    // 4-2) exp : 6bit 니까 <<5.
     unsigned short int exp = e + BIAS;
     exp <<= 5;
+
+    // 4-3) frac
+
 
     fp12 result = 0; // 결과 저장할 fp12형 16bit를 0으로 초기화
 
